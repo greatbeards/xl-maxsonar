68fdf6b43a8bf4a7066d65accd4e73eed40cca4d
Great Beards <greli.lubbeek@gmail.com>
2022-06-22 10:28:34 +0200

    testing component init

--- custom_components/XL_MaxSonar/__init__.py
+++ custom_components/XL_MaxSonar/__init__.py
@@ -9,20 +9,36 @@ _LOGGER = logging.getLogger(__name__)
 
 from .xl_maxsonar import XLMaxSonar
 from .const import DOMAIN
+import serial_asyncio
+import asyncio
 
 PLATFORMS: list[str] = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the XL-max sonar sensor component."""
 
-    server = XLMaxSonar(serial_port="/dev/ttyAMA0", baudrate=9600, timeout=10)
+    serial_port="/dev/ttyAMA0"
+    baudrate=9600
+    timeout=10
+
+    loop = asyncio.get_event_loop()
+    async def reader(server):
+        
+        transport, protocol = await serial_asyncio.create_serial_connection(loop, server, serial_port, baudrate=baudrate)
+
+        while True:
+            await asyncio.sleep(0.3)
+            protocol.resume_reading()
+
+    server = XLMaxSonar()
+
     hass.data[DOMAIN] = server
 
     #load sensors
     hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
 
     #run the server 
-    hass.async_add_job(server.run())
+    hass.async_add_job(reader(server))
 
     return True
 
